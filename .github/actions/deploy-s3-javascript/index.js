f7d8d0efe53696558cb753f75485a4acbd01bc44
bishal189 <bishalmurmu150@gmail.com>
2025-07-20 09:44:25 +0545

    feat:no aws account so commented

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -1,9 +1,16 @@
 const core = require("@actions/core");
+const exe = require("@actions/exec");
 
 async function run() {
   try {
-    const name = core.getInput("name");
-    console.log(`Hello, ${name}!`);
+    // getting the info
+    const bucketName = core.getInput("bucket-name", { required: true });
+    const bucketRegion = core.getInput("bucketRegion", { required: true });
+    const distFolder = core.getInput("distFolder", { required: true });
+
+    //uploading to the s3 server
+    const s3Uri = `s3://${bucketName}`;
+    exe.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
   } catch (error) {
     core.setFailed(error.message);
   }
